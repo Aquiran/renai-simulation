7fefae50a200986c5dfc0e67edf77ce73098a385
Aquiran <103098617+Aquiran@users.noreply.github.com>
2022-11-11 11:59:33 +0900

    Add files via upload

---
+++ 전태성 연애시뮬레이션/title.js
@@ -0,0 +1,8 @@
+function danger() {
+    window.location = "ranai.html"
+}
+
+function start()   {
+    document.getElementById("risk").style.display = "block"
+    document.getElementById("black").style.display = "block"
+}
